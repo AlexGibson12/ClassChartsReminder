7ad2973b6fdbe5c49d71d9c200a9b8fcf688d90f
AlexGibson12 <67548304+AlexGibson12@users.noreply.github.com>
2021-01-20 15:23:05 +0000

    Update classchartsreminder.py

--- classchartsreminder.py
+++ classchartsreminder.py
@@ -4,27 +4,37 @@ import time
 import arrow
 import datetime
 from plyer import notification
-secrets = open("secrets.txt","r").readlines()
-CLASSCHARTS_AUTHORIZATION = secrets[0].strip()
-CLASSCHARTS_COOKIE = secrets[1].strip()
+classcharts_home_url = "https://www.classcharts.com/"
+classcharts_api_url = posixpath.join(classcharts_home_url, "apiv2student")
+classcharts_login_url = posixpath.join(classcharts_api_url, "login")
+classcharts_timetable_url = posixpath.join(classcharts_api_url, "timetable")
+CLASSCHARTS_CODE = "YOURCODEHERE"
+CLASSCHARTS_DOB = "DD/MM/YYYY"
 while True:
-    headers = {
-    'authorization': CLASSCHARTS_AUTHORIZATION,
-    'cookie': CLASSCHARTS_COOKIE
-    }
     params = (('date', arrow.now().format('YYYY-MM-DD')),)
-    response = requests.get(
-        'https://www.classcharts.com/apiv2student/timetable/',
-        headers=headers,
-        params=params
-     ).text
-    for lesson in json.loads(response)["data"]:
+
+    login = requests.post(classcharts_login_url, cookies={
+        "cc-session": requests.get(classcharts_home_url).cookies["cc-session"]
+    }, data={
+        "_method": "POST",
+        "code": CLASSCHARTS_CODE,
+        "dob": CLASSCHARTS_DOB,
+        "remember_me": "true",
+        "recaptcha-token": "no-token-available"
+    }).json()
+
+    timetable = requests.get(classcharts_timetable_url, headers={
+        "Authorization": f'Basic {login["meta"]["session_id"]}'
+    }, params=params).json()["data"]
+
+    for lesson in timetable:
         now = datetime.datetime.now()
         start = datetime.datetime.strptime(
-        lesson["start_time"], '%Y-%m-%dT%H:%M:%S+00:00')
+            lesson["start_time"], '%Y-%m-%dT%H:%M:%S+00:00')
         if now.hour == start.hour and now.minute == start.minute:
-            notification.notify( 
-            title = "ClassCharts", 
-            message=f'You have {lesson["subject_name"]} now', 
-            timeout=5) 
+            notification.notify(
+                title="ClassCharts",
+                message=f'You have {lesson["subject_name"]} now',
+                timeout=5)
     time.sleep(60)
+
